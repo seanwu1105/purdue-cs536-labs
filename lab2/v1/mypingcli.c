3cc8634e64774ec401eee25eec8ec4d77c80eb71
Shuang Wu <seanwu1105@gmail.com>
2021-09-26 20:39:47 -0400

    Receive pinging feedback.

--- lab2/v1/mypingcli.c
+++ lab2/v1/mypingcli.c
@@ -8,9 +8,17 @@
 
 #define REQUIRED_ARGC 4
 
-int ping(int sockfd, struct sockaddr *target_addr, int32_t id, uint8_t delay)
+int sockfd;
+
+void tear_down()
+{
+    close(sockfd);
+}
+
+int send_pinging(const struct sockaddr *target_addr, const int32_t id, const uint8_t delay)
 {
     uint8_t message[MESSAGE_LEN];
+    printf("sending: id=%d, delay=%hu", id, delay);
     encode_message(id, delay, message);
     if (sendto(sockfd, message, sizeof(message), 0, target_addr, sizeof(*target_addr)) == -1)
     {
@@ -21,10 +29,44 @@ int ping(int sockfd, struct sockaddr *target_addr, int32_t id, uint8_t delay)
     return 0;
 }
 
-int run(int sockfd, struct sockaddr *target_addr, Config config)
+int receive_feedback()
+{
+    uint8_t message[MESSAGE_LEN];
+    struct sockaddr originating_addr;
+    socklen_t originating_addr_len = sizeof(originating_addr);
+
+    ssize_t message_len = recvfrom(sockfd, message, sizeof(message), 0, &originating_addr, &originating_addr_len);
+
+    if (message_len == -1)
+    {
+        perror("recvfrom");
+        return -1;
+    }
+
+    if (message_len == 0)
+        return 0;
+
+    int32_t id;
+    uint8_t delay;
+    decode_message(message, &id, &delay);
+
+    printf("received: id=%d, delay=%hu\n", id, delay);
+    return 0;
+}
+
+int ping(const struct sockaddr *target_addr, int32_t id, uint8_t delay)
+{
+    if (send_pinging(target_addr, id, delay) == -1)
+        return -1;
+    if (receive_feedback() == -1)
+        return -1;
+    return 0;
+}
+
+int run(const struct sockaddr *target_addr, Config config)
 {
     for (size_t i = 0; i < config.num_packages; i++)
-        if (ping(sockfd, target_addr, config.first_sequence_num + (int32_t)i, config.server_delay) == -1)
+        if (ping(target_addr, config.first_sequence_num + (int32_t)i, config.server_delay) == -1)
             return -1;
 
     return 0;
@@ -50,6 +92,12 @@ int parse_arg(int argc, char *argv[], struct addrinfo **client_info, struct addr
     return 0;
 }
 
+void signal_handler(int _)
+{
+    tear_down();
+    exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char *argv[])
 {
     Config config = {};
@@ -58,7 +106,7 @@ int main(int argc, char *argv[])
     if (parse_arg(argc, argv, &client_info, &server_info) != 0 || read_config(&config) == -1)
         return -1;
 
-    int sockfd = create_socket_with_first_usable_addr(server_info);
+    sockfd = create_socket_with_first_usable_addr(server_info);
     if (sockfd == -1)
         return -1;
 
@@ -68,7 +116,7 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    run(sockfd, server_info->ai_addr, config);
+    run(server_info->ai_addr, config);
 
     freeaddrinfo(client_info);
     freeaddrinfo(server_info);
