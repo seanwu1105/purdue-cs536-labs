976230bbfd58ce440aca8ba3edba6315ef2600ae
Shuang Wu <seanwu1105@gmail.com>
2021-09-27 20:56:11 -0400

    WIP: initializing lab2/v2.

---
+++ lab2/v2/rcommandclient.c
@@ -0,0 +1,78 @@
+#include <fcntl.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define COMMAND_SIZE PIPE_BUF * 2
+
+void tear_down()
+{ /* close socket */
+}
+
+static void sigint_handler(int _)
+{
+    tear_down();
+    exit(EXIT_SUCCESS);
+}
+
+int run()
+{
+    while (1)
+    {
+        // send command to server
+        char command[COMMAND_SIZE];
+        fprintf(stdout, "> ");
+        if (!fgets(command, sizeof(command), stdin)) break;
+
+        char prefixed_command[COMMAND_SIZE + 100];
+        snprintf(prefixed_command, sizeof(prefixed_command), "%d\n%s", getpid(),
+                 command);
+
+        size_t len = strlen(prefixed_command) + 1;
+
+        int server_fifo_fd = open(SERVER_FIFO_NAME, O_WRONLY);
+        if (server_fifo_fd == -1)
+        {
+            fprintf(stderr, "Cannot open FIFO: %s\n", SERVER_FIFO_NAME);
+            return -1;
+        }
+
+        if (len * sizeof(char) <= PIPE_BUF)
+            write(server_fifo_fd, prefixed_command, len * sizeof(char));
+        else
+            fprintf(stderr, "Command length too long.\n");
+        close(server_fifo_fd);
+
+        // read result from server
+        int client_fifo_fd = open(client_fifo_name, O_RDONLY);
+        if (client_fifo_fd == -1) return -1;
+
+        char buf[PIPE_BUF];
+        ssize_t result_len;
+        while (result_len = read(client_fifo_fd, buf, PIPE_BUF), result_len > 0)
+        {
+            buf[result_len] = '\0';
+            fprintf(stdout, "%s", buf);
+        }
+
+        close(client_fifo_fd);
+        if (result_len == -1) return -1;
+    }
+    return 0;
+}
+
+int main()
+{
+    struct sigaction sigint_action = {.sa_handler = sigint_handler};
+    sigaction(SIGINT, &sigint_action, NULL);
+
+    int status = run();
+
+    tear_down();
+
+    return status;
+}
---
+++ lab2/v2/rcommandserver.c
@@ -0,0 +1,109 @@
+// Simple shell example using fork() and execlp().
+
+#include "parse_command.h"
+#include <fcntl.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int server_fifo_fd;
+
+void tear_down()
+{
+    close(server_fifo_fd);
+    unlink(SERVER_FIFO_NAME);
+}
+
+int start_server()
+{
+    pid_t k;
+    char buf[PIPE_BUF];
+    int status;
+
+    while (1)
+    {
+        // read command from FIFO
+        ssize_t command_len = read(server_fifo_fd, buf, PIPE_BUF);
+        if (command_len == -1)
+            return -1;
+        else if (command_len == 0) // EOF
+            continue;              // busy wait for the new command from clients
+
+        char *pid_str = strtok(buf, "\n");
+        char *command = strtok(NULL, "\0");
+
+        if (!pid_str || !command) return -1;
+
+        fprintf(stdout, "[%s]$ %s", pid_str, command);
+
+        char *arguments[PIPE_BUF];
+        parse_command(command, arguments);
+
+        int client_fifo_fd;
+
+        fflush(stdout); // flush stdout before forking
+        k = fork();
+        if (k == 0)
+        {
+            // child code
+
+            // open client FIFO according to the parsed pid
+            char client_fifo_filename[100] = CLIENT_FIFO_NAME_PREFIX;
+            strcat(client_fifo_filename, pid_str);
+            client_fifo_fd = open(client_fifo_filename, O_WRONLY);
+            if (client_fifo_fd == -1) return -1;
+            // redirect stdout to client FIFO file descriptor
+            if (dup2(client_fifo_fd, STDOUT_FILENO) == -1 ||
+                dup2(client_fifo_fd, STDERR_FILENO) == -1)
+                exit(EXIT_FAILURE);
+
+            int result = execvp(arguments[0], arguments);
+
+            if (result == -1) // if execution failed, terminate child
+            {
+                fprintf(stderr, "Command not found: %s", command);
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            // parent code
+            waitpid(k, &status, 0);
+            close(client_fifo_fd);
+        }
+    }
+
+    return 0;
+}
+
+static void sigint_handler(int _)
+{
+    tear_down();
+    exit(EXIT_SUCCESS);
+}
+
+int main()
+{
+    struct sigaction sigint_action = {.sa_handler = sigint_handler};
+    sigaction(SIGINT, &sigint_action, NULL);
+
+    if (mkfifo(SERVER_FIFO_NAME, S_IRUSR | S_IWUSR | S_IWGRP | S_IWOTH) == -1)
+        return -1;
+    server_fifo_fd = open(SERVER_FIFO_NAME, O_RDONLY);
+    if (server_fifo_fd == -1)
+    {
+        unlink(SERVER_FIFO_NAME);
+        return -1;
+    }
+
+    int status = start_server();
+
+    tear_down();
+
+    return status;
+}
