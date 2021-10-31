377cbdd8731f5778c162fb98d42854fff1fd5228
Shuang Wu <seanwu1105@gmail.com>
2021-10-30 21:16:11 -0400

    Build packet codec.

---
+++ lab4/v1/packet_codec.c
@@ -0,0 +1,17 @@
+#include "packet_codec.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+void encode_packet(const uint8_t num, const uint8_t *const data,
+                   const size_t data_size, uint8_t *const packet)
+{
+    packet[0] = num;
+    memcpy(packet + 1, data, data_size);
+}
+void decode_packet(const uint8_t *const packet, uint8_t *const num,
+                   const size_t data_size, uint8_t *const data)
+{
+    *num = packet[0];
+    memcpy(data, packet + 1, data_size);
+}
---
+++ lab4/v1/packet_codec.h
@@ -0,0 +1,12 @@
+#if !defined(_PACKET_CODEC_H_)
+#define _PACKET_CODEC_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+void encode_packet(const uint8_t num, const uint8_t *const data,
+                   const size_t data_size, uint8_t *const packet);
+void decode_packet(const uint8_t *const packet, uint8_t *const num,
+                   const size_t data_size, uint8_t *const data);
+
+#endif // _PACKET_CODEC_H_
