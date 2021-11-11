a3bfeae51d197372902b54bad633f6e3c6c6d9bc
Shuang Wu <seanwu1105@gmail.com>
2021-11-10 23:03:17 -0500

    Update test audio `ahoy.au`.

--- lab5/v1/pspacing.c
+++ lab5/v1/pspacing.c
@@ -4,7 +4,8 @@
 
 uint16_t to_pspacing_ms(const long double packets_per_second)
 {
-    if (packets_per_second < 0) return 0;
     if (packets_per_second == 0) return MAX_PSPACING_MS;
-    return (uint16_t)(1000.0 / packets_per_second);
+    uint16_t packet_interval_ms = (uint16_t)(1000.0 / packets_per_second);
+    return packet_interval_ms > MAX_PSPACING_MS ? MAX_PSPACING_MS
+                                                : packet_interval_ms;
 }
